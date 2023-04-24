76955ff4b9c4a0dfc2a7858f14ef01d9ff6011ec
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-04-24 14:26:37 +0900

    update

--- app.py
+++ app.py
@@ -18,19 +18,17 @@ def get_media(id, post_id):
     media = json["media"]
     return media
 
-
-def show_kakao_posts(channel_id):
-    posts = get_posts(channel_id)
-    for post in posts['items']:
-        st.write('===')
-        st.write('작성 시간:', post['createdAt'])
-        st.write('내용:', post['content'])
-        media = get_media(channel_id, post['id'])
-        if media:
-            st.write('이미지:')
-            for m in media:
-                if 'xlarge_url' in m:
-                    st.image(m['xlarge_url'])
+def show_store(store_id):
+    posts = get_posts(store_id)
+    post_ids = [p['id'] for p in posts['items']]
+    images = []
+    for post_id in post_ids:
+        media = get_media(store_id, post_id)
+        for m in media:
+            images.append(m['xlarge_url'])
+    cols = st.columns(min(3, len(images)))
+    for i, col in enumerate(cols[:3]):
+        col.image(images[i])
 
 
 st.set_page_config(
@@ -45,6 +43,6 @@ st.metric(
     label="현재일자",
     value=now.strftime("%Y-%m-%d"))
 st.write("---")
-st.write("### ㅎㅇ")
 
-show_kakao_posts("_fQxhxdz")
+show_store("_fQxhxdz")
+
